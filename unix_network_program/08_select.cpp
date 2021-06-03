75c83a09c7a96101cfefeb02e5361f15a6b05858
machun <302806766@qq.com>
2021-06-03 22:46:46 +0800

    完善select笔记和代码

--- unix_network_program/08_select.cpp
+++ unix_network_program/08_select.cpp
@@ -7,49 +7,26 @@
 #include "common.h"
 
 
-int new_connect_handle()
-{
-	struct sockaddr_in tClientAddr;
-	memset(&tClientAddr, 0, sizeof(tClientAddr));
-
-	nAddrLen = sizeof(tClientAddr);
-	nAcceptFd = accept(nListenFd, &tClientAddr, &nAddrLen);
-	if (-1 == nAcceptFd)
-	{
-		printf("accept failed, error:%s\n", strerror(errno));
-		return -1;
-	}
-
-	inet_ntop(AF_INET, &tClientAddr.sin_addr, achAddr, sizeof(achAddr));
-	printf("new client comes. peer addr %s:%d\n", achAddr, htons(tClientAddr.sin_port));
-	return 0;
-}
-
-int recv_handle()
-{
-
-	return 0;
-}
-
 int main()
 {
+	s32 i = 0;
 	s32 nRet = 0;
 	s32 nListenFd = 0;
 	s32 nAcceptFd = 0;
 	s32 nMaxFd = 0;
 	s32 nOpt = 0;
-	s32 nAddrLen = 0;
-	fd_set tReadFdSet;
-	fd_set tOldFdSet;
+	socklen_t nAddrLen = 0;
+	fd_set tReadSet;
+	fd_set tOldSet;
 	s8  achAddr[64] = { 0 };
+	s8  achRecvBuf[1500+1] = { 0 };
 	struct sockaddr_in tServerAddr;
-	struct timeval tTimeOut;
+	struct sockaddr_in tClientAddr;
 
-	memset(&tTimeOut, 0, sizeof(tTimeOut));
-	memset(&tReadFdSet,  0, sizeof(tReadFdSet));
-	memset(&tOldFdSet,   0, sizeof(tOldFdSet));
+	memset(&tReadSet,  0, sizeof(tReadSet));
+	memset(&tOldSet,   0, sizeof(tOldSet));
 	memset(&tServerAddr, 0, sizeof(tServerAddr));
-	
+	memset(&tClientAddr, 0, sizeof(tClientAddr));
 
 	nListenFd = socket(AF_INET, SOCK_STREAM, 0);
 	if (-1 == nListenFd)
@@ -63,8 +40,8 @@ int main()
 	setsockopt(nListenFd, SOL_SOCKET, SO_REUSEADDR, &nOpt, sizeof(nOpt));
 
 	tServerAddr.sin_family = AF_INET;
-	tServerAddr.sin_addr = INADDR_ANY;
 	tServerAddr.sin_port = htons(TCP_SERVER_PORT);
+	tServerAddr.sin_addr.s_addr = 0;
 	nRet = bind(nListenFd, (struct sockaddr*)&tServerAddr, sizeof(tServerAddr));
 	if (-1 == nRet)
 	{
@@ -79,30 +56,78 @@ int main()
 		return -1;
 	}
 
-	// 超时时间 3s
-	tTimeOut.tv_sec = 3;
-	tTimeOut.tv_usec = 0;
-
 	// 文件描述符集合处理
-	FD_ZERO(&tOldFdSet);
-	FD_ZERO(&tReadFdSet); 
-	FD_SET(nListenFd, &tReadFdSet);
-	tOldFdSet = tReadFdSet;
-	nMaxFd = nListenFd + 1;
+	FD_ZERO(&tOldSet);
+	FD_ZERO(&tReadSet); 
+	FD_SET(nListenFd, &tOldSet);
+	nMaxFd = nListenFd;
 
 	while (1)
 	{
-		nRet = select(nMaxFd, &tReadFdSet, NULL, NULL, &tTimeOut);
+		tReadSet = tOldSet;
+
+		nRet = select(nMaxFd + 1, &tReadSet, NULL, NULL, NULL);
 		if (nRet > 0) // 有文件可读
 		{
-			if (FD_ISSET(nListenFd, &tReadFdSet))
-			{
-				new_connect_handle();
-			}
+			printf("%d fd change", nRet);
 
-			if ()
+			// 这里的返回值其实没用，因为只知道有几个文件描述符属性变化，不知道具体是哪个，
+			// 还需要通过遍历的方式找到属性变化的文件描述符，如果中间有很多文件描述符没用，其实效率很低。
+			// epoll相比较而言，有个参数可以将文件描述符传进去，这样就不需要去遍历，可以提高效率
+			// 这里需要遍历所有文件描述符
+			for (i = 1; i < nMaxFd; i++)
 			{
-				recv_handle();
+				// 可以自定义一个数组，将文件描述符保存到这个数组里！！！
+				// 不需要遍历所有文件描述符，只需要遍历数组的有效元素即可
+				if (FD_ISSET(i, &tReadSet))
+				{
+					if (i == nListenFd) // 有新的连接
+					{
+						nAddrLen = sizeof(tClientAddr);
+						nAcceptFd = accept(nListenFd, (struct sockaddr*)&tClientAddr, &nAddrLen);
+						if (-1 == nAcceptFd)
+						{
+							printf("accept failed, error:%s\n", strerror(errno));
+						}
+						else
+						{
+							inet_ntop(AF_INET, &tClientAddr.sin_addr, achAddr, sizeof(achAddr));
+							printf("new client comes. peer addr %s:%d\n", achAddr, ntohs(tClientAddr.sin_port));
+
+							// 添加到监听文件描述符集合
+							FD_SET(nAcceptFd, &tOldSet);
+							if (nAcceptFd > nMaxFd)
+							{
+								nMaxFd = nAcceptFd;
+							}
+						}
+					}
+					else // 有数据可读 
+					{
+						nRet = recv(i, achRecvBuf, (size_t)sizeof(achRecvBuf), 0);
+						if (nRet == 0) // 对方关闭
+						{
+							printf("client closed\n");
+
+							// 从监听的文件描述符集合中清除，并关闭该socket
+							FD_CLR(i, &tOldSet);
+							close(i);
+							continue;
+						}
+						else if (nRet < 0) // 出错
+						{
+							printf("recv failed, error:%s\n", strerror(errno));
+
+							FD_CLR(i, &tOldSet);
+							close(i);
+							continue;
+						}
+						else // 接收到数据
+						{
+							printf("recv %s\n", achRecvBuf);
+						}
+					}					
+				}
 			}
 		}
 		else if (nRet == -1) // 出错
